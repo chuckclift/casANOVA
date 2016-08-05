c5a895dca6cdbf709028eaa660632db4365e110b
Chuck Clift <chuckcclift@gmail.com>
2016-08-05 11:53:08 -0400

    added functions to take averages

--- calculate.js
+++ calculate.js
@@ -21,3 +21,33 @@ function add_column() {
         c.innerHTML = "<input type=\"text\">"; 
     }
 }
+
+function row_means() {
+    var table = document.getElementById("calcTable"); 
+    var columns = table.rows[0].children.length; 
+    var rows = table.rows.length; 
+    var means = []; 
+
+    for (var i = 0; i < rows; i++) means.push(mean_of_row(table.rows[i], 1)); 
+
+
+    for (var m=0; m<means.length; m++) {console.log("mean: ", means[m]); }
+}
+
+function mean_of_row(row, start) {
+    var columns = row.children.length; 
+
+    var sum = 0;
+    var count = 0; 
+
+    for (var i=start; i<columns; i++) {
+        var cell = row.children[i].children[0].value; 
+            if (parseFloat(cell)) {
+                sum += parseFloat(cell); 
+                count += 1; 
+                console.log("added: ", cell); 
+            }         
+    } 
+    
+    return sum / count; 
+}
