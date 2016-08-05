72f88c41b10e589087530d8e979f7ef0b1f235d3
Chuck Clift <chuckcclift@gmail.com>
2016-08-04 22:16:22 -0400

    made add_row function more readable

--- calculate.js
+++ calculate.js
@@ -1,14 +1,12 @@
 function add_row() {
     var table = document.getElementById("calcTable"); 
-    var tablesize = table.rows.length; 
-    var row = table.insertRow(tablesize);
+    var total_rows = table.rows.length; 
 
+    var row = table.insertRow(total_rows);
     var columns = table.rows[0].children.length; 
 
-    var cells = []; 
-    for (i = 0; i < columns; i++) {cells.push(row.insertCell(0)); }
-
-    cells[columns - 1].innerHTML = "<input type=\"text\" value=\"cat" + tablesize +  "\">"
-    for (i = 0; i < columns - 1; i++) { cells[i].innerHTML = "<input type=\"text\">";}
+    for (var i = 0; i < columns; i++) {
+        var c = row.insertCell(0); 
+        c.innerHTML = "<input type=\"text\">" ; 
+    }
 }
-
